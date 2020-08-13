6c82c0784e318d6cd50593181f2d4aaf206ee9ba
JelsianaJennet <jelsianajennet@gmail.com>
2020-08-14 01:06:58 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,69 +1,73 @@
 
 import { SkynetClient } from "skynet-js";
 
-//const client =new SkynetClient();
-
-window.createMediaPage = function (mainMediaFile){
-    const pageContent =''
-
-    const mediaFolder ={
-        "index.html": new File(),
-        "media.jpg" : mainMediaFile
-    }
-    try{
-        (async() => {
-            const {skyLink} = await client.uploadDirectory(mediaFolder, 'mediaFolder')
-            let directlink ="/"+skylink+"/";
-            this.document.getElementById("mediaLink").href= directlink;
-            this.document.getElementById("mediaLink").text=skyLink;
-        }) ();
-        }
-        catch(error){
-            this.console.log(error);
-    }
-    
-}
-/*
+const client = new SkynetClient("https://siasky.net");
 const name = "testcreateskykey";
+// const myskykey ="skykey:AUI0eAOXWXHwW6KOLyI5O1OYduVvHxAA8qUR_fJ8Kluasb-ykPlHBEjDczrL21hmjhH0zAoQ3-Qq"
 
-async function createSkykeyExample() {
-  try {
-    const client = new SkynetClient();
-    const skykey = await client.createSkykey(name, "private-id");
-  } catch (error) {
-    console.log(error)
-  }
-}*/
-
-window.createSkykeyExample = async function (s) {
-    const name = "testcreateskykey";
-    try{
-        
-            const client =new SkynetClient();
-            const skykey = await client.createSkykey(name, "private-id");
-            console.log(skykey);
-        
-    }catch(error){
-        alert(error);
-        console.log(error);
+window.display = function (show) {
+  (async() => {
+    if (show == 'display-Upload'){
+      document.getElementById('upload').style.display='block';
+      document.getElementById('download').style.display='none';
     }
+    if (show == 'display-Download'){
+      document.getElementById('download').style.display='block';
+      document.getElementById('upload').style.display='none';
+    }
+  }) ();
+  
+  return false;
+}
+window.uploadFileEncryption = function(file , skykey) {
+
+if(file == null){
+  this.alert("Please send the file to upload")
 }
 
-window.uploadFileEncryption = async function(file,skyKeyName) {
     try {
-      const client = new SkynetClient();
-      const { skylink } = await client.upload(file, { skykeyName: skyKeyName });
-      this.console.log(skylink);
+      (async() => {
+        if(skykey == null || skykey == ""){
+          if(document.getElementById('skylinkMsg').text == ""){
+            document.getElementById('skylinkMsg').text = "Please Enter SkyKey or Select Upload again to proceed with deafult Skykey.";
+            return;
+          }
+          else{
+            skykey = "AUI0eAOXWXHwW6KOLyI5O1OYduVvHxAA8qUR_fJ8Kluasb-ykPlHBEjDczrL21hmjhH0zAoQ3-Qq";
+          }      
+          
+        }
+
+        const { skylink } = await client.upload(file,{skykey : skykey});
+        document.getElementById('skylinkDisplay').text = "Skylink :" + skylink;
+        document.getElementById('skylinkMsg').text = "SkyKey : " + skykey;
+        console.log(`Upload successful, skylink: ${skylink}`);
+      }) ();
     } catch (error) {
       console.log(error);
     }
   }
 
-  window.downloadFileEncryption = async function(skylink){
-    //const skylink = "XABvi7JtJbQSMAcDwnUnmp2FKDPjg8_tTTFP4BwMSxVdEg";
+  window.downloadFileEncryption = function(skylink , skykey){
+
+    if(skylink == "" && skykey == ""){
+      alert("Please Enter the SkyKey & SkyLink to download");
+      return;
+    }
+    if(skylink == ""){
+      alert("Please Enter the SkyLink to download");
+      return;
+    }
+    if(skykey == ""){
+      alert("Please Enter the SkyKey to proceed");
+      return;
+    }
+
     try {
-      const client = new SkynetClient();
-      client.download(skylink);
+      (async () => {
+        await client.download(skylink,{skykey : skykey});
+        document.getElementById('skylinkMsg2').text = "Download Successful"
+    })();
     } catch (error) {
       console.log(error)
     }
